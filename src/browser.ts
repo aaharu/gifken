a7174dd964cfd26203c78703d922e4bea36aee23
Haruaki OTAKE <aaharu@hotmail.com>
2019-10-14 18:49:59 +0900

    rebuild br rollup
    BREAKING CHANGE

---
+++ src/browser.ts
@@ -0,0 +1,8 @@
+import { Gif } from "./Gif";
+import { GifColor } from "./GifColor";
+import { GifFrame } from "./GifFrame";
+import { GifParser } from "./GifParser";
+import { GifVersion } from "./GifVersion";
+import { GifPresenter } from "./GifPresenter";
+
+export { Gif, GifColor, GifFrame, GifParser, GifVersion, GifPresenter };
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Gif } from "./Gif";
+import { GifColor } from "./GifColor";
+import { GifFrame } from "./GifFrame";
+import { GifParser } from "./GifParser";
+import { GifVersion } from "./GifVersion";
+
+export { Gif, GifColor, GifFrame, GifParser, GifVersion };
